ce2442877224482eae5b2ffba35efb5bf81ab427
MohdAhmad1 <dexk2015@gmail.com>
2023-10-22 12:57:24 +0530

    frontend setup

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button, Text } from "@mantine/core";
+
+function App() {
+  return (
+    <>
+      <Text>Click on the Vite and React logos to learn more</Text>
+
+      <Button>Hello</Button>
+    </>
+  );
+}
+
+export default App;
