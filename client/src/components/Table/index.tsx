d293b598a3c98251447ff60f5d26f7e57f862e8e
MohdAhmad1 <dexk2015@gmail.com>
2023-10-22 16:16:04 +0530

    added table component

---
+++ client/src/components/Table/index.tsx
@@ -0,0 +1,179 @@
+import { useState } from "react";
+import {
+  Table,
+  ScrollArea,
+  UnstyledButton,
+  Group,
+  Text,
+  Center,
+  TextInput,
+  rem,
+  keys,
+} from "@mantine/core";
+import {
+  IconSelector,
+  IconChevronDown,
+  IconChevronUp,
+  IconSearch,
+} from "@tabler/icons-react";
+import classes from "./Table.module.css";
+
+type RowData = Record<string, string | number>;
+
+interface ThProps {
+  children: React.ReactNode;
+  reversed: boolean;
+  sorted: boolean;
+  onSort(): void;
+}
+
+function Th({ children, reversed, sorted, onSort }: ThProps) {
+  const Icon = sorted
+    ? reversed
+      ? IconChevronUp
+      : IconChevronDown
+    : IconSelector;
+  return (
+    <Table.Th className={classes.th}>
+      <UnstyledButton onClick={onSort} className={classes.control}>
+        <Group justify="space-between">
+          <Text fw={500} fz="sm">
+            {children}
+          </Text>
+          <Center className={classes.icon}>
+            <Icon style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
+          </Center>
+        </Group>
+      </UnstyledButton>
+    </Table.Th>
+  );
+}
+
+function filterData(data: RowData[], search: string) {
+  const query = search.toLowerCase().trim();
+  return data.filter((item) =>
+    keys(data[0]).some((key) => String(item[key]).toLowerCase().includes(query))
+  );
+}
+
+function sortData(
+  data: RowData[],
+  payload: { sortBy: keyof RowData | null; reversed: boolean; search: string }
+) {
+  const { sortBy } = payload;
+
+  if (!sortBy) {
+    return filterData(data, payload.search);
+  }
+
+  return filterData(
+    [...data].sort((a, b) => {
+      if (payload.reversed) {
+        return String(b[sortBy]).localeCompare(String(a[sortBy]));
+      }
+
+      return String(a[sortBy]).localeCompare(String(b[sortBy]));
+    }),
+    payload.search
+  );
+}
+
+interface Props<T extends RowData[]> {
+  data: T;
+  accessors: Array<keyof T[number]>;
+}
+
+function TableSort<T extends RowData[]>({ data, accessors }: Props<T>) {
+  const [search, setSearch] = useState("");
+  const [sortedData, setSortedData] = useState(data);
+  const [sortBy, setSortBy] = useState<keyof RowData | null>(null);
+  const [reverseSortDirection, setReverseSortDirection] = useState(false);
+
+  const setSorting = (field: keyof RowData) => {
+    const reversed = field === sortBy ? !reverseSortDirection : false;
+    setReverseSortDirection(reversed);
+    setSortBy(field);
+
+    setSortedData(
+      sortData(data as unknown as RowData[], {
+        sortBy: field,
+        reversed,
+        search,
+      }) as unknown as T
+    );
+  };
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget;
+    setSearch(value);
+    setSortedData(
+      sortData(data as unknown as RowData[], {
+        sortBy,
+        reversed: reverseSortDirection,
+        search: value,
+      }) as unknown as T
+    );
+  };
+
+  const rows = sortedData.map((row) => (
+    <Table.Tr key={row.id}>
+      {accessors.map((accessor) => (
+        <Table.Td key={accessor.toString()}>{row[accessor]}</Table.Td>
+      ))}
+    </Table.Tr>
+  ));
+
+  return (
+    <ScrollArea>
+      <TextInput
+        placeholder="Search by any field"
+        mb="md"
+        leftSection={
+          <IconSearch
+            style={{ width: rem(16), height: rem(16) }}
+            stroke={1.5}
+          />
+        }
+        value={search}
+        onChange={handleSearchChange}
+      />
+      <Table
+        horizontalSpacing="md"
+        verticalSpacing="xs"
+        miw={700}
+        layout="fixed"
+      >
+        <Table.Thead>
+          <Table.Tr>
+            {accessors.map((accessor) => (
+              <Th
+                sorted={sortBy === String(accessor).toLowerCase()}
+                reversed={reverseSortDirection}
+                onSort={() => setSorting(String(accessor).toLowerCase())}
+                key={String(accessor)}
+              >
+                {String(accessor)}
+              </Th>
+            ))}
+          </Table.Tr>
+        </Table.Thead>
+
+        <Table.Tbody>
+          {rows.length > 0 ? (
+            rows
+          ) : (
+            <Table.Tr>
+              <Table.Td colSpan={Object.keys(data[0]).length}>
+                <Text fw={500} ta="center">
+                  Nothing found
+                </Text>
+              </Table.Td>
+            </Table.Tr>
+          )}
+        </Table.Tbody>
+      </Table>
+    </ScrollArea>
+  );
+}
+
+export { TableSort as Table };
--- client/src/pages/Movies.tsx
+++ client/src/pages/Movies.tsx
@@ -1,5 +1,21 @@
-function Movies() {
-  return <div>Movies</div>;
-}
+import { Table } from "../components/Table";
 
-export default Movies;
+export default function Movies() {
+  return (
+    <div>
+      Movies
+      <br />
+      <br />
+      <br />
+      <Table
+        data={[
+          { id: "22", name: "23Ahmad", x: 4 },
+          { id: "1", name: "Mohd Ahmad", x: 3 },
+          { id: 2, name: "D", x: 1 },
+          { id: 3, name: "e", x: 0 },
+        ]}
+        accessors={["name", "x"]}
+      />
+    </div>
+  );
+}
